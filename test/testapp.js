2b2967fd9fc4020b66be6c6f47d4336b258f7083
Daniel Deutsch <deudan1010@gmail.com>
2016-11-24 19:15:46 +0100

    adding webserver test in mocha

--- test/testapp.js
+++ test/testapp.js
@@ -15,7 +15,7 @@ describe("Mocha/Chai", function(){
 
 describe("App funcitonality", function(){
   var url = "http://localhost:3000/";
-  it("returns status 200", function(done) {
+  it("basic response returns status 200", function(done) {
     request(url, function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
