4f32789ecb1c7b10acdba2602d327b4afc3942a9
Daniel Deutsch <deudan1010@gmail.com>
2016-11-24 15:48:09 +0100

    express introduction

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 var express=require('express');//returns a function
-var bodyParser = require('body-parser');
+var bodyParser=require('body-parser');
 var app=express();
+var port=process.env.PORT || 3000;
 
+app.listen(port);
+console.log("app is listening on port: "+port);
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.set("view engine", "ejs");
-app.use("/assets", express.static("assets"));
-app.get("/", function(req, res){
-  res.render("index");
-});
-app.get("/contact", function(req, res){
-  res.render("contact",{qs:req.query});
-});
-app.post("/contact", urlencodedParser, function(req, res){
-  console.log(req.body);
-  res.render("contact-success",{data: req.body});
-});
-app.get("/profile/:name", function(req, res){
-  var data = {age:29, job: "truck", hobbies:["eating", "fighting"]};
-  res.render("profile", {person:req.params.name, data: data});
-});
-app.listen(3000);
-console.log("app is listening on port 3000");
+app.get("/",(req,res) => {
+  res.send("läuft!");
+})
+app.get("/contact",(req,res) =>{
+  res.send("contact"+this);
+})
