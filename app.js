92a1fc0d50b1d1bd23b3b24818da2afae79665c2
Daniel Deutsch <deudan1010@gmail.com>
2016-11-24 15:59:29 +0100

    adding jshint

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* jshint node: true */
+/*jshint esversion: 6 */
+"use strict";
+
 var express=require('express');//returns a function
 var bodyParser=require('body-parser');
 var app=express();
@@ -8,7 +12,7 @@ console.log("app is listening on port: "+port);
 
 app.get("/",(req,res) => {
   res.send("läuft!");
-})
+});
 app.get("/contact",(req,res) =>{
   res.send("contact"+this);
-})
+});
